fd0d653defebf275355bfc317a8ae9e8f51d2cff
ankita_muddapur <ankita_muddapur@cms.co.in>
2020-03-12 15:26:31 +0530

    Adding .cs class with functionality modification.

--- Program.cs
+++ Program.cs
@@ -10,33 +10,25 @@ namespace EvenOddArrayConsoleApplication
     {
         static void Main(string[] args)
         {
-            int[] arrayElements = new int[10];
-            int[] Array = new int[10] ;
-            int[] EvenElements = new int[5];
-            int[] OddElements = new int[5];
+            int[] arrayElements = new int[10];          
+            int EvenCount = 0, OddCount = 0;  
             Console.Write( "Enter the array elements: " );
-            
-            for (int i = 0 ; i< arrayElements.Length ; i++)
+
+            for (int i = 0; i <= (arrayElements.Length - 1); i++)
             {
-              Array[i] = Convert.ToInt32( Console.ReadLine() );
+                arrayElements[i] = Convert.ToInt32(Console.ReadLine());
+                if (arrayElements[i] % 2 == 0)
+                {
+                    EvenCount ++;
+                }
+                else if (arrayElements[i] % 2 != 0)
+                {
+                    OddCount ++;                  
+                }               
             }
-
-          for(int j=0 ; j<Array.Length; j++)
-          {
-              if (Array[j] % 2 == 0)
-              {
-                   EvenElements[j] = Array[j];
-              }
-              else
-              {
-                   OddElements[j] = Array[j];
-              }
-          }
-          Console.Write( "Even elements in array is: " + EvenElements );
-          Console.ReadLine();
-
-          Console.Write( "Odd elements in array is: " + OddElements );
-          Console.ReadLine();
+            Console.Write( "Even elements in array is: " + EvenCount);          
+            Console.Write( "Odd elements in array is: " + OddCount);
+            Console.ReadLine();
 
         }
     }
